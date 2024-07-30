bb79700d486de5fb7b422df4005c3410de98f59b
naomimariuta <157979968+naomimariuta@users.noreply.github.com>
2024-07-30 03:07:42 +0300

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,9 @@
-const ENDPOINT = 'https://api.thecatapi.com/v1/';
+const ENDPOINT = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_iptdsI1Ru8L4LETQYtOgAfDFEvUzpau4IsYRg2MZd2sQbR8lTXcllFLtcbXwMyfH';
 
 export function fetchBreeds() {
-  return fetch(`${ENDPOINT}breeds?api_key=${API_KEY}`).then(response => {
+  return fetch(`${ENDPOINT}/breeds?api_key=${API_KEY}`).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
@@ -13,7 +13,7 @@ export function fetchBreeds() {
 
 export function fetchCatByBreed(breedId) {
   return fetch(
-    `${ENDPOINT}images/search?api_key=${API_KEY}&breed_ids=${breedId}`
+    `${ENDPOINT}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
   ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
